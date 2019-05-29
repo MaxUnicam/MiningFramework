eb60314fd20591d69b12c292d0c60636073280f4
Massimiliano <massimilian.sampaolo@studenti.unicam.it>
2019-05-29 23:35:37 +0200

    Release of the web app with the file configuration

---
+++ web-client/dist/MiningFramework/assets/config.js
@@ -0,0 +1,7 @@
+(function (window) {
+    window.__env = window.__env || {};
+
+    window.__env.apiEndpoint = 'http://localhost:8080';
+    window.__env.ethereumEndpoint = 'https://mainnet.infura.io/v3/419c7f687f3b41f0bc9e0e647b9ab911';
+
+}(this));
